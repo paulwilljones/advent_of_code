c4936965448531fa8ae650a19d1ad7b6956304c9
pwjones89 <parker1>
2015-12-15 19:25:54 +0000

    Init commit of day 2

---
+++ day2/Day2Solution.py
@@ -0,0 +1,16 @@
+
+class Day2Solution(object):
+
+    def calculate_area(self, length, width, height):
+
+        try:
+            surface_area_1 = length * width
+            surface_area_2 = width * height
+            surface_area_3 = height * length
+        except ValueError:
+            print "Values are not integers"
+            raise
+
+        return (2 * surface_area_1) + (2 * surface_area_2) + \
+               (2 * surface_area_3) + \
+               min(surface_area_1, surface_area_2, surface_area_3)
---
+++ day2/main.py
@@ -0,0 +1,18 @@
+from Day2Solution import Day2Solution
+
+def main():
+
+    solution = Day2Solution()
+    total_area = 0
+    with open("input.txt") as f:
+        for line in f:
+            line = line.rstrip().split("x")
+            area = solution.calculate_area(length=int(line[0]), width=int(line[1]), height=int(line[2]))
+            total_area += area
+
+    print total_area
+
+    return
+
+if __name__ == "__main__":
+    main()
